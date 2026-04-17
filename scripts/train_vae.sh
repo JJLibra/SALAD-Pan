98a85234236ad4fd3977998d12e0c19a06ee03d1
xxfer <2565474095@qq.com>
2026-04-17 13:56:00 +0800

    🐞 fix: config path

--- scripts/train_vae.sh
+++ scripts/train_vae.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-accelerate launch --config_file config/accelerate.yaml train_vae.py --config config/train_vae.yaml
+accelerate launch --config_file configs/accelerate.yaml train_vae.py --config configs/train_vae.yaml
